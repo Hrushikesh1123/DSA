3ce7445636b39365426920a59fbf93ac12e07b13
Hrushikesh1123 <infohrushikesh@gmail.com>
2023-10-23 18:44:48 +0530

    added

---
+++ DSA/src/basicMaths/CountDigits.java
@@ -0,0 +1,18 @@
+package basicMaths;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int number=input.nextInt();
+        //need to print how many digits are present in the number
+        int totalDigits=0;
+        while(number>0){
+            totalDigits++;
+            number/=cd 10;
+        }
+        System.out.println("Total digits present in this number are :"+ totalDigits);
+
+    }
+}
