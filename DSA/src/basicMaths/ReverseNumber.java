f76a770319516a7c7bfe50693f8544cd2d4acb72
Hrushikesh1123 <infohrushikesh@gmail.com>
2023-10-24 10:07:00 +0530

    changes are added

---
+++ DSA/src/basicMaths/ReverseNumber.java
@@ -0,0 +1,20 @@
+package basicMaths;
+
+public class ReverseNumber {
+    //reverse a number -2**31 to 2**31
+    public int reverse(int x) {
+        if(x==0)return 0;
+        int reversedString=0;
+        boolean isPositive=x>0?true:false;
+        x=x>=0?x:(-1*x);
+        //x=Math.abs(x);
+        while(x>0){
+            reversedString*=10;
+
+            int digit=x%10;
+            x/=10;
+            reversedString+=digit;
+        }
+        return isPositive==true?reversedString:-(reversedString);
+    }
+}
