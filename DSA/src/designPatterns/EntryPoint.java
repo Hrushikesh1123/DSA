28c335fdaaa88e96014d80e6d80de72b1973bc1e
Hrushikesh1123 <infohrushikesh@gmail.com>
2024-05-23 20:09:53 +0530

    added thread safty

--- DSA/src/designPatterns/EntryPoint.java
+++ DSA/src/designPatterns/EntryPoint.java
@@ -6,13 +6,10 @@ public class EntryPoint {
 
     public static void main(String[] args) throws IOException {
         Logger logger=Logger.getInstance();
-        logger.logInfo("Hi");
+        logger.logInfo("Hidd");
         logger.logError("hello");
         logger.logWarn("warn");
+        logger.close();
         System.out.println("Executed successfully");
-        Logger logger1=Logger.getInstance();
-        if(logger.equals(logger1)){
-            System.out.println("Both share the same address");
-        }
     }
 }
